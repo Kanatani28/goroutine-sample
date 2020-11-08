9ed200ac38d33d779c2eb7822a1b6682417d678c
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2020-11-08 17:12:22 +0900

    add goroutine sample

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("# main() Start")
+	alphabetList := []string{"A", "B", "C", "D", "E"}
+	for _, alphabet := range alphabetList {
+		// goroutineとして実行
+		go sub(alphabet)
+	}
+	time.Sleep(time.Second * 2)
+	fmt.Println("# main() End")
+}
+
+// goroutineで実行する関数
+func sub(val string) {
+	fmt.Println("## sub() " + val)
 }
