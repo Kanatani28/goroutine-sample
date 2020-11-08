c8e980dcd317b4bde802b1e8b42f7a674c29a3d3
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2020-11-08 17:14:31 +0900

    add WaitGroup sample

--- main.go
+++ main.go
@@ -2,21 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
 	fmt.Println("# main() Start")
 	alphabetList := []string{"A", "B", "C", "D", "E"}
+	// ① WaitGroupを宣言する
+	wg := new(sync.WaitGroup)
 	for _, alphabet := range alphabetList {
-		// goroutineとして実行
-		go sub(alphabet)
+		// ② 終了待ちするgoroutineの数を設定していく（今回の場合最終的に5になる）
+		wg.Add(1)
+		// ③ goroutineとして実行
+		go sub(alphabet, wg)
 	}
-	time.Sleep(time.Second * 2)
+	// ⑤ WaitGroupに設定された数だけDoneが実行されるまで待機
+	wg.Wait()
 	fmt.Println("# main() End")
 }
 
-// goroutineで実行する関数
-func sub(val string) {
+// goroutineで実行する関数（引数にWaitGroup追加）
+func sub(val string, wg *sync.WaitGroup) {
+	// ④ 処理終了時にDoneメソッド実行するようにする
+	defer wg.Done()
 	fmt.Println("## sub() " + val)
 }
